17a317ccf471f1ed9ae76deec92788ddb312a549
devin-bielejec <devin.bieleje@gmail.com>
2019-10-01 13:53:41 -0400

    post request to insert username password works

--- db.js
+++ db.js
@@ -2,16 +2,19 @@ const knex = require("knex");
 const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 
-module.exports = {
-    insertUser,
-    getAllUsers
-}
 
-const insertUser = (username, password) => {
+
+const insertUser = (user) => {
+    console.log(user);
     return db("users")
-    .insert({ username: username, password: password})
+    .insert(user)
 }
 
 const getAllUsers = () => {
     return db("users");
+}
+
+module.exports = {
+    insertUser,
+    getAllUsers
 }
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const bcrypt = require("bcryptjs");
+const Users = require("./db");
+
 dotenv.config();
 
 const server = express();
-
+server.use(express.json());
 const PORT = process.env.PORT || 1;
 
+//middleware
+
+
 //api calls
+server.post("/users", (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    Users.insertUser({username: username, password: hash})
+    .then(res => {
+        console.log('chicken');
+        res.status(200).json({message: "User was insertion successful!"})    
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`))
